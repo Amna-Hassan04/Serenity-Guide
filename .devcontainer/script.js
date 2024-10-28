44d5eee8ada3806caedb51f39dc98e844bbe8493
VJ12More <150460035+VJ12More@users.noreply.github.com>
2024-10-28 19:44:04 +0530

    Add files via upload

---
+++ .devcontainer/script.js
@@ -0,0 +1,7 @@
+// Toggle navbar menu on smaller screens
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
